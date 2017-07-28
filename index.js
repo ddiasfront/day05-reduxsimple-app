1c1c2aba6cf5223138aafe4a25b953190d50a447
Diego Dias <diegodias@diegodias.local>
2017-07-27 21:53:23 -0300

    redux app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 //REDUCER
-function counter(state={ count: 0 }, action) {
+function counterReducer(state={ count: 0 }, action) {
 
   let nextState = {
     count: currentState.count
@@ -22,31 +22,38 @@ function counter(state={ count: 0 }, action) {
   }
 }
 //TOD-O REDUCER
-function todosReducer(state, action) {
-  if (typeof state === 'undefined') {
-    return { todos: [] }
-    }
+// function todosReducer(state, action) {
+//   if (typeof state === 'undefined') {
+//     return { todos: [] }
+//     }
 
-  let nextState = Object.assign({}, state);
+//   let nextState = Object.assign({}, state);
 
-  switch (action.type) {
-    case 'NEW':
-      nextState.todos.push(action.payload)
-      return nextState;
-    case 'DELETE':
-      nextState.todos.pop();
-      return nextState;
-    case 'DELETEALL':
-      nextState.todos = []
-      return nextState;
-      break;
-    default:
-      return state
-  }
+//   switch (action.type) {
+//     case 'NEW':
+//       nextState.todos.push(action.payload)
+//       return nextState;
+//     case 'DELETE':
+//       nextState.todos.pop();
+//       return nextState;
+//     case 'DELETEALL':
+//       nextState.todos = []
+//       return nextState;
+//       break;
+//     default:
+//       return state
+//   }
+// }
+
+const logger = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
 }
 
 //STORE
-const store = createStore(todosReducer);
+const store = createStore(counterReducer);
 const counterEl = document.getElementById('counter');
 const todosInput = document.getElementById('todos');
 const todolist = document.getElementById('todolist');
@@ -56,14 +63,14 @@ function render() {
   // counterEl.innerHTML = state.count.toString()
   renderList(state);
 }
-function renderList(state) {
-  todolist.innerHTML = '';
-  state.todos.map(function(todo, i) { 
-    let li = document.createElement('li');
-    li.innerHTML = todo.toString()
-    todolist.appendChild(li);
-  });
-}
+// function renderList(state) {
+//   todolist.innerHTML = '';
+//   state.todos.map(function(todo, i) { 
+//     let li = document.createElement('li');
+//     li.innerHTML = todo.toString()
+//     todolist.appendChild(li);
+//   });
+// }
 
 store.subscribe(render)
 
@@ -83,17 +90,17 @@ document.getElementById('reset')
       store.dispatch({ type: 'RESET' })
     })
 
-document.getElementById('new')
-    .addEventListener('click', () => {
-      store.dispatch({ type: 'NEW', payload: todosInput.value })
-    })
+// document.getElementById('new')
+//     .addEventListener('click', () => {
+//       store.dispatch({ type: 'NEW', payload: todosInput.value })
+//     })
 
-document.getElementById('delete')
-    .addEventListener('click', () => {
-      store.dispatch({ type: 'DELETE' })
-    })
+// document.getElementById('delete')
+//     .addEventListener('click', () => {
+//       store.dispatch({ type: 'DELETE' })
+//     })
 
-document.getElementById('deleteall')
-    .addEventListener('click', () => {
-      store.dispatch({ type: 'DELETEALL' })
-    })
+// document.getElementById('deleteall')
+//     .addEventListener('click', () => {
+//       store.dispatch({ type: 'DELETEALL' })
+//     })
